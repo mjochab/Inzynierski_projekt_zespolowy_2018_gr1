69e152960a2335e92d516f9e439361a4adc2ac2d
Bartek <Bartek@DESKTOP-9FU5MM2>
2018-12-09 12:23:48 +0100

    Okno alertu IEGR1-41

--- przychodnia/src/przychodnia/Alerty.java
+++ przychodnia/src/przychodnia/Alerty.java
@@ -43,11 +43,11 @@ public class Alerty extends Application {
         Label blad = new Label("Nieprawidłowy login lub hasło.\nPopraw błędy i spróbuj ponownie");
         GridPane.setConstraints(blad, 1, 2);
         
-        FileInputStream znak = new FileInputStream("img/blad.png");
+        /*FileInputStream znak = new FileInputStream("img/blad.png");
         Image image = new Image(znak);
         ImageView imageView = new ImageView(image);
         Label label = new Label("Znak", imageView);
-        GridPane.setConstraints(label, 1, 0);
+        GridPane.setConstraints(label, 1, 0);*/
         
         //button
         Button button = new Button("Ok"); 
@@ -59,7 +59,7 @@ public class Alerty extends Application {
         grid.setVgap(8);
         grid.setHgap(10);
                 
-        grid.getChildren().addAll(label, napis, blad, button);
+        grid.getChildren().addAll(/*label, */napis, blad, button);
         
         scene = new Scene(grid, 350, 180);
         window.setScene(scene);
