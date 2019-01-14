1b2e13d8b47d6fc3f2026e5c3aef277943c56f21
Krystian Tracz <krysystian31@gmail.com>
2019-01-14 22:34:53 +0100

    testy jednostkowe

---
+++ przychodnia/test/przychodnia/PacjenciTest.java
@@ -0,0 +1,243 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package przychodnia;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Krystian Tracz
+ */
+public class PacjenciTest {
+    
+    public PacjenciTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getid_Pacjenta method, of class Pacjenci.
+     */
+    @Test
+    public void testGetid_Pacjenta() {
+        System.out.println("getid_Pacjenta");
+        Pacjenci instance = null;
+        int expResult = 0;
+        int result = instance.getid_Pacjenta();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getName method, of class Pacjenci.
+     */
+    @Test
+    public void testGetName() {
+        System.out.println("getName");
+        Pacjenci instance = null;
+        String expResult = "";
+        String result = instance.getName();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getSurname method, of class Pacjenci.
+     */
+    @Test
+    public void testGetSurname() {
+        System.out.println("getSurname");
+        Pacjenci instance = null;
+        String expResult = "";
+        String result = instance.getSurname();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getPESEL method, of class Pacjenci.
+     */
+    @Test
+    public void testGetPESEL() {
+        System.out.println("getPESEL");
+        Pacjenci instance = null;
+        int expResult = 0;
+        int result = instance.getPESEL();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getAdres method, of class Pacjenci.
+     */
+    @Test
+    public void testGetAdres() {
+        System.out.println("getAdres");
+        Pacjenci instance = null;
+        String expResult = "";
+        String result = instance.getAdres();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getTelepon method, of class Pacjenci.
+     */
+    @Test
+    public void testGetTelepon() {
+        System.out.println("getTelepon");
+        Pacjenci instance = null;
+        int expResult = 0;
+        int result = instance.getTelepon();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getE_mail method, of class Pacjenci.
+     */
+    @Test
+    public void testGetE_mail() {
+        System.out.println("getE_mail");
+        Pacjenci instance = null;
+        String expResult = "";
+        String result = instance.getE_mail();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getHaslo method, of class Pacjenci.
+     */
+    @Test
+    public void testGetHaslo() {
+        System.out.println("getHaslo");
+        Pacjenci instance = null;
+        String expResult = "";
+        String result = instance.getHaslo();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setId_Pacjeta method, of class Pacjenci.
+     */
+    @Test
+    public void testSetId_Pacjeta() {
+        System.out.println("setId_Pacjeta");
+        int id = 0;
+        Pacjenci instance = null;
+        instance.setId_Pacjeta(id);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setName method, of class Pacjenci.
+     */
+    @Test
+    public void testSetName() {
+        System.out.println("setName");
+        String names = "";
+        Pacjenci instance = null;
+        instance.setName(names);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setSurnmae method, of class Pacjenci.
+     */
+    @Test
+    public void testSetSurnmae() {
+        System.out.println("setSurnmae");
+        String surnames = "";
+        Pacjenci instance = null;
+        instance.setSurnmae(surnames);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setPESEL method, of class Pacjenci.
+     */
+    @Test
+    public void testSetPESEL() {
+        System.out.println("setPESEL");
+        Integer pesel = null;
+        Pacjenci instance = null;
+        instance.setPESEL(pesel);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setTelephon method, of class Pacjenci.
+     */
+    @Test
+    public void testSetTelephon() {
+        System.out.println("setTelephon");
+        Integer telephons = null;
+        Pacjenci instance = null;
+        instance.setTelephon(telephons);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setEmail method, of class Pacjenci.
+     */
+    @Test
+    public void testSetEmail() {
+        System.out.println("setEmail");
+        String email = "";
+        Pacjenci instance = null;
+        instance.setEmail(email);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setHaslo method, of class Pacjenci.
+     */
+    @Test
+    public void testSetHaslo() {
+        System.out.println("setHaslo");
+        String haslo1 = "";
+        Pacjenci instance = null;
+        instance.setHaslo(haslo1);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
